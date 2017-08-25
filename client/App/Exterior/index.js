b7d7a90bb7644bd643478ab8bbe44a2d34d051e7
Travis CI User <travis@example.org>
2017-08-25 17:22:31 +0000

    publish: chore(readme): added a basic README
    generated from commit 3e67812e1db802df42ba2b63f7d6e32298ea023f

--- client/App/Exterior/index.js
+++ client/App/Exterior/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 
 export default ({children}) => (
   <Layout>
-    <AppBar title='Project Starter' showMenuIconButton={false} />
+    <AppBar title='Starter' showMenuIconButton={false} />
     {children}
   </Layout>
 );
